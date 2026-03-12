536bc967698b5fbe6d2a34513b2527d417583005
EndOverdosing <endoverdosing@gmail.com>
2026-03-11 20:19:31 -0700

    Update 99-Nights-in-the-Forest.lua

--- 99-Nights-in-the-Forest.lua
+++ 99-Nights-in-the-Forest.lua
@@ -1,3 +1,5 @@
+// yes this is open sourced, yes the key is EndOverdosing
+
 local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
 local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
 local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
